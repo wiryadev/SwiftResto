b334d9ac18e80607378f6d777ec36ba339081b36
wiryadev <abrarwiryawan@gmail.com>
2023-01-11 09:34:48 +0700

    Initial Commit

---
+++ SwiftResto/SwiftRestoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftRestoApp.swift
+//  SwiftResto
+//
+//  Created by Abrar Wiryawan on 11/01/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftRestoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
